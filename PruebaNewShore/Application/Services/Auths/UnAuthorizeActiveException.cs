0bfdd3c986a297455c9c462e2bab4671eccc888a
CristianOrtega12 <cristian.r.ortega.c@gmail.com>
2023-12-02 10:00:00 -0500

    Change version and Name

---
+++ PruebaNewShore/Application/Services/Auths/UnAuthorizeActiveException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Application.Services.Auths
+{
+    [Serializable]
+    internal class UnAuthorizeActiveException : Exception
+    {
+        public UnAuthorizeActiveException()
+        {
+        }
+
+        public UnAuthorizeActiveException(string message) : base(message)
+        {
+        }
+
+        public UnAuthorizeActiveException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected UnAuthorizeActiveException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
